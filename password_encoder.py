2f8f36c46f0ae361969403cefcd51a3935b267d2
Jmcgraww <johnnymcgraw0301@gmail.com>
2023-03-09 13:42:34 -0500

    Added encoder program to repository.

---
+++ password_encoder.py
@@ -0,0 +1,28 @@
+#Made by John McGraw
+def encode(password):
+    encodedpassword = ''
+    for element in password:
+        element = int(element) + 3
+        encodedpassword = encodedpassword + str(element)
+
+    return encodedpassword
+
+
+def main():
+    userOption = 0
+    while not userOption == 3:
+        print("Menu\n-------------\n1. Encode\n2. Decode\n3. Quit\n")
+        userOption = int(input("Please enter an option: "))
+        if userOption == 1:
+            password = str(input("Please enter your password to encode: "))
+            encodedpassword = encode(password)
+            print("Your password has been encoded and stored!\n")
+        elif userOption == 2:
+            print("The encoded password is " + encodedpassword + ", and the original password is " + password + ".")
+        elif userOption == 3:
+            break
+
+
+
+if __name__ == "__main__":
+    main()
